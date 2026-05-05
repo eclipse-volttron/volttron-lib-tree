a79697cec5a25ab7f080064aeb083ae90818f391
Chandrika <schandrika@users.noreply.github.com>
2026-05-05 11:19:47 -0700

    Revert "Modernize modular topic tree library and update metadata"

--- src/volttron/lib/tree/__init__.py
+++
@@ -1,4 +0,0 @@
-from . device import DeviceNode, DeviceTree
-from . topic import DuplicatedNodeIdError, NodeIDAbsentError, TopicNode, TopicTree
-
-__all__ = DeviceNode, DeviceTree, DuplicatedNodeIdError, NodeIDAbsentError, TopicNode, TopicTree
